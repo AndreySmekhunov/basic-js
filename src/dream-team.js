3d23729633639b1711b7c83efa05292c22074af1
AndreySmekhunov <smekhunov@gmail.com>
2022-10-11 19:59:10 +0300

    feat: add dream team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,11 +13,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(arr) {
+  if (!Array.isArray(arr)) return false;
+  arr = arr.filter(a => typeof a == 'string');
+  for (let i = 0; i < arr.length; i++)
+    arr[i] = arr[i].trim().toUpperCase()[0];
+  return arr.sort().join('');
 }
-
 module.exports = {
   createDreamTeam
 };
